c768c7b03b83983e2f76ee569159e1345ce45b05
Quy Nguyen <quy.nguyen@parknplug.fr>
2016-05-03 10:46:02 +0200

    Init

---
+++ src/Algorithms_Box_1_0.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Algorithms_Box_1_0.cpp
+// Author      : Nhan-Quy NGUYEN
+// Version     :
+// Copyright   : Copyright Park'n'Plug 2016
+// Description : Algorithms box 1.0 which helps to getting events from GO
+//				 and analyzing the request to create suitable input file for
+//				 the chosen algorithm
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Algoriths box 1.0" << endl; // prints Algoriths box 1.0
+	return 0;
+}
