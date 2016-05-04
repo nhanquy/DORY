31ec1564efe8fa2c9bba901a5ceea566cd136efe
Quy Nguyen <quy.nguyen@parknplug.fr>
2016-05-04 16:50:56 +0200

    Chages EV

--- src/EV.cpp
+++ src/EV.cpp
@@ -9,18 +9,135 @@
 
 EV::EV() :
 		auth_time(0), charging_power(0), borne_id(0), user_id(0), starting_time(
-				0), charging(false), fully_charged(false), est_demand(0) {
+				0), charging(false), fully_charged(false), halted(false), est_demand(
+				0), last_changing_time(0), total_energy(0) {
 }
 
 EV::EV(int m_user_id, int m_borne_id, double m_auth_time,
 		double m_charging_power) :
 		user_id(m_user_id), borne_id(m_borne_id), auth_time(m_auth_time), charging_power(
-				m_charging_power), charging(false), fully_charged(false), est_demand(0){
+				m_charging_power), charging(false), fully_charged(false), halted(
+				false), starting_time(0), est_demand(0), last_changing_time(0), total_energy(
+				0) {
 
 }
 
+EV::EV(const EV& that) {
+	auth_time = that.auth_time;
+	charging_power = that.charging_power;
+	borne_id = that.borne_id;
+	user_id = that.user_id;
+	starting_time = that.starting_time;
+	charging = that.charging;
+	fully_charged = that.fully_charged;
+	est_demand = that.est_demand;
+	last_changing_time = that.last_changing_time;
+	total_energy = that.total_energy;
+	halted = that.halted;
+}
+
+EV& EV::operator =(const EV& that) {
+	auth_time = that.auth_time;
+	charging_power = that.charging_power;
+	borne_id = that.borne_id;
+	user_id = that.user_id;
+	starting_time = that.starting_time;
+	charging = that.charging;
+	fully_charged = that.fully_charged;
+	est_demand = that.est_demand;
+	last_changing_time = that.last_changing_time;
+	total_energy = that.total_energy;
+	halted = that.halted;
+	return *this;
+}
 
 EV::~EV() {
-	// TODO Auto-generated destructor stub
 }
 
+/*
+ *  Setting parameters
+ */
+
+void EV::set_user_id(idInt m_value) {
+	user_id = m_value;
+}
+
+void EV::set_auth_time(double m_value) {
+	auth_time = m_value;
+}
+void EV::set_borne_id(idInt m_value) {
+	borne_id = m_value;
+}
+
+void EV::set_est_demand(const char* string_date) {
+	// TODO: Referencing predicted value in database by date-time string
+}
+
+/*
+ *  Getting informations
+ */
+
+const int EV::get_user_id() {
+	return user_id;
+}
+const int EV::get_borne_id() {
+	return borne_id;
+}
+const double EV::get_auth_time() {
+	return auth_time;
+}
+const double EV::get_charging_power() {
+	return charging_power;
+}
+const double EV::get_est_demand() {
+	return est_demand;
+}
+
+/*
+ *  Events definitions
+ */
+
+void EV::begin_to_charge(double m_power, double m_stime) {
+	starting_time = m_stime;
+	charging_power = m_power;
+	last_changing_time = m_stime;
+	total_energy = 0;
+	charging = true;
+}
+
+void EV::changing_charge_power(double new_charging_power, double change_time) {
+	if (!charging) {
+		std::cout << "Cannot modify a charge hasn't been started yet! Error!\n";
+		return;
+	}
+	total_energy += charging_power * (change_time - last_changing_time);
+	charging_power = new_charging_power;
+	last_changing_time = change_time;
+}
+
+void EV::end_of_charge(double e_stime) {
+	if (!charging) {
+		std::cout << "Cannot end a charge hasn't been started yet! Error!\n";
+		return;
+	}
+	total_energy += charging_power * (e_stime - last_changing_time);
+	last_changing_time = e_stime;
+	charging_power = 0;
+	fully_charged = true;
+	charging = false;
+}
+
+void EV::interupting(double set_time) {
+	if (!charging) {
+		std::cout << "Cannot end a charge hasn't been started yet! Error!\n";
+		return;
+	}
+	if (fully_charged) {
+		std::cout << "Cannot interrupt a charge has been completely charged! Error!\n";
+		return;
+	}
+	total_energy += charging_power * (set_time - last_changing_time);
+	last_changing_time = set_time;
+	charging = false;
+	halted = true;
+}
--- src/EV.h
+++ src/EV.h
@@ -7,33 +7,37 @@
 
 #ifndef EV_H_
 #define EV_H_
+#include "TimeRef.h"
 
 class EV {
 public:
 	EV(); 												// Create new EV
 	EV(int m_user_id, int m_borne_id, double m_auth_time,
-			double m_charging_power);					// Add new EV with carr arrival event
+			double m_charging_power);	// Add new EV with carr arrival event
 	EV(const EV& that);
 	EV& operator=(const EV& that);
 	~EV();
 
+	// Setting parameters
+
 	void set_user_id(idInt m_value);			// Unsigned int
 	void set_auth_time(double m_value); // Double, number of hours since starting of scheduling horizon
 	void set_borne_id(idInt m_value);			// Unsigned int
-	void set_charging_power(double m_value);	// Double, in kW
-	void set_est_demand(double m_value);		// Double, in kW.h
+	void set_est_demand(const char* string_date);		// Double, in kW.h
+
+	// Getting informations
 
 	const int get_user_id();
 	const int get_borne_id();
-	const double get_parking_time();
+	const double get_auth_time();
 	const double get_charging_power();
 	const double get_est_demand();
 
-	// Events												// Reference code
-	void begin_to_charge(double set_charging_power);		// ref BOC
-	void changing_charge_power(double new_charging_power); 	// ref PID
-	void end_of_charge();									// ref FDC
-	void interupting();										// ref ANU
+	// Events																	// Reference code
+	void begin_to_charge(double m_power, double m_stime);			// ref BOC
+	void changing_charge_power(double new_charging_power, double change_time); // ref PID
+	void end_of_charge(double e_stime);								// ref FDC
+	void interupting(double set_time);								// ref ANU
 	void pause();
 
 	// Getting charging information
@@ -47,9 +51,12 @@ private:
 	idInt borne_id, user_id;
 	double starting_time;
 	// Status flag
-	bool charging, fully_charged;
+	bool charging, fully_charged, halted;
 	// Estimation
 	double est_demand;
+	// Energy management
+	double last_changing_time;
+	double total_energy;
 };
 
 #endif /* EV_H_ */
